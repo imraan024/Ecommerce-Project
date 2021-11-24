1aae791437d42a6f19456fc19fc500ccfc6d2f45
imraan024 <asq24i@gmail.com>
2021-11-24 23:36:01 +0600

    pagination works

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import Products
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def home_view(request):
     context = {}
-    products = Products.objects.all()
-    context['products'] = products
+    products = Products.objects.get_queryset().order_by('id')
+    paginator = Paginator(products, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'products' : page_obj,
+        'page_obj' : page_obj
+    }
     return render(request, "home.html", context)
